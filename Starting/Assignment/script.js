9563a949700541b5c2d73990588502aa5fd10b20
Shubhangi172001 <shubhangikompelli7060@gmail.com>
2024-03-13 13:08:07 +0530

    initial

---
+++ Starting/Assignment/script.js
@@ -0,0 +1,27 @@
+//first code
+    // var a = confirm('weather you want to proceed further or not')
+    // if( a==true )
+    // {
+    //     document.getElementById('new').innerHTML = 'you can proceed further.......'
+    // }
+    // else
+    // {
+    //     document.getElementById('new').innerHTML = 'you canot proceed further.......'
+    // }
+
+
+//second code
+    // var text = ""
+    // var a = ""
+    // for (let i = 0; i < 5; i++)
+    // {
+    //     a = text + "The number is "+ i + "<br>"
+    //     console.log(a)
+    // }
+
+
+//third code
+    function fun()
+    {
+        
+    }
